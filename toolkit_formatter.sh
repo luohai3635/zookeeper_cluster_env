9b123d4735ae850a1aba80b31bc2610a6f28473f
Luo Hai <lhasdfr@gmail.com>
2020-03-30 23:36:07 +0800

    update batch

--- toolkit_formatter.sh
+++ toolkit_formatter.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-operatation=$*
-java -Djava.ext.dirs=formatter_lib org.apache.zookeeper.server.persistence.TxnLogToolkit $operatation
+deletezxids=$1
+filename=$2
+java -Djava.ext.dirs=formatter_lib org.apache.zookeeper.server.persistence.TxnLogToolkit -drv  -s $1 $2
